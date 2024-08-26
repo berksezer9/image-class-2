4d6d6707866588627c1200180d320b2714609064
berksezer9 <berksezer9@gmail.com>
2024-08-26 22:40:09 +0300

    feat: add copy of the code from the article

---
+++ article.py
@@ -0,0 +1,243 @@
+import torch
+import torchvision
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data import random_split
+
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+
+#train and test data directory
+data_dir = "./resources/seg_train"
+test_data_dir = "./resources/seg_test"
+
+
+#load the train and test data
+dataset = ImageFolder(data_dir,transform = transforms.Compose([
+    transforms.Resize((150,150)),transforms.ToTensor()
+]))
+test_dataset = ImageFolder(test_data_dir,transforms.Compose([
+    transforms.Resize((150,150)),transforms.ToTensor()
+]))
+
+img, label = dataset[0]
+
+#output :
+#torch.Size([3, 150, 150]) 0
+
+
+#output:
+#Follwing classes are there :
+# ['buildings', 'forest', 'glacier', 'mountain', 'sea', 'street']
+
+def display_img(img,label):
+    print(f"Label : {dataset.classes[label]}")
+    plt.imshow(img.permute(1,2,0))
+
+batch_size = 8
+val_size = 2000
+train_size = len(dataset) - val_size
+
+train_data,val_data = random_split(dataset,[train_size,val_size])
+print(f"Length of Train Data : {len(train_data)}")
+print(f"Length of Validation Data : {len(val_data)}")
+
+#output
+#Length of Train Data : 12034
+#Length of Validation Data : 2000
+
+
+# @ToDo, we may use a generator later for reproducibility
+#load the train and validation into batches.
+train_dl = DataLoader(train_data, batch_size, shuffle = True, pin_memory = True)
+val_dl = DataLoader(val_data, batch_size*2, pin_memory = True)
+
+
+def show_batch(dl):
+    """Plot images grid of single batch"""
+    for images, labels in dl:
+        fig, ax = plt.subplots(figsize=(16, 12))
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.imshow(make_grid(images, nrow=16).permute(1, 2, 0))
+        plt.show()
+        break
+
+
+# show_batch(train_dl)
+
+def check_gradient_flow(model):
+    # Store the gradients in a list
+    ave_grads = []
+    max_grads = []
+    layers = []
+
+    # Loop over all layers of the model
+    for name, param in model.named_parameters():
+        if param.requires_grad and "bias" not in name:
+            layers.append(name)
+            ave_grads.append(param.grad.abs().mean().item())
+            max_grads.append(param.grad.abs().max().item())
+
+    # Plotting the gradients
+    plt.figure(figsize=(10, 5))
+    plt.plot(ave_grads, alpha=0.3, color="b", label="Average gradient")
+    plt.plot(max_grads, alpha=0.3, color="r", label="Maximum gradient")
+    plt.hlines(0, 0, len(ave_grads) + 1, linewidth=1, color="k")
+    plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(xmin=0, xmax=len(ave_grads))
+    plt.xlabel("Layers")
+    plt.ylabel("Gradient magnitude")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.legend(loc="upper right")
+    plt.show()
+
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    for epoch in range(epochs):
+
+        model.train()
+        train_losses = []
+
+        bn = 1 # the batch count
+
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+
+            # once every 20 batches
+            if bn % 20 == 1:
+                # Check gradient flow
+                check_gradient_flow(model)
+
+            optimizer.step()
+            optimizer.zero_grad()
+
+            print(f"batch no: {bn}, loss: {loss.item()}")
+
+            bn += 1
+
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        model.epoch_end(epoch, result)
+        history.append(result)
+
+    return history
+
+
+class ImageClassificationBase(nn.Module):
+
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        return loss
+
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        acc = accuracy(out, labels)  # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['train_loss'], result['val_loss'], result['val_acc']))
+
+
+class NaturalSceneClassification(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(
+
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Flatten(),
+            nn.Linear(82944, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 6)
+        )
+
+    def forward(self, xb):
+        return self.network(xb)
+
+num_epochs = 30
+opt_func = torch.optim.Adam
+lr = 0.001
+model = NaturalSceneClassification()# assumes this line
+#fitting the model on training data and record the result after each epoch
+history = fit(num_epochs, lr, model, train_dl, val_dl, opt_func)
+
+
+def plot_accuracies(history):
+    """ Plot the history of accuracies"""
+    accuracies = [x['val_acc'] for x in history]
+    plt.plot(accuracies, '-x')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.title('Accuracy vs. No. of epochs')
+    plt.show()
+
+
+plot_accuracies(history)
+
+
+def plot_losses(history):
+    """ Plot the losses in each epoch"""
+    train_losses = [x.get('train_loss') for x in history]
+    val_losses = [x['val_loss'] for x in history]
+    plt.plot(train_losses, '-bx')
+    plt.plot(val_losses, '-rx')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(['Training', 'Validation'])
+    plt.title('Loss vs. No. of epochs')
+    plt.show()
+
+
+plot_losses(history)
